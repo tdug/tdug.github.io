b3ae2497b4ecc9d8057f9d6d4a86c3505ae1c5dc
Tom Dugovic <tom.dugovic@gmail.com>
2020-03-20 16:30:05 -0500

    Updates

---
+++ precache-manifest.ca2b97d49e46ddbec55df621d8109a66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "663059d97f441efa087ee7006110da35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73273158c99f1c1639c0",
+    "url": "/static/js/2.5d806d45.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5d806d45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56b9b90e230ca8546b56",
+    "url": "/static/js/main.489f9c9b.chunk.js"
+  },
+  {
+    "revision": "3522f02304c8a6bb494f",
+    "url": "/static/js/runtime-main.f8bf88a0.js"
+  }
+]);
